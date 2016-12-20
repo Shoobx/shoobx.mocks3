8d9498cd0d19e0cbd73bae2e13eccc506711e419
Stephan Richter <stephan.richter@gmail.com>
2016-12-20 16:36:27 -0500

    Provide a better bucket name.

--- scripts/performance.py
+++ scripts/performance.py
@@ -7,12 +7,12 @@ import time
 s3 = boto3.resource(
     's3', region_name='us-east-1', endpoint_url=sys.argv[1])
 
-bucket = s3.Bucket('test')
+bucket = s3.Bucket('com.shoobx.app.perf.test')
 bucket.create()
 
 t1 = time.time()
 
-data = 'Hello World! '*100000
+data = 'Hello World! '*1000
 
 for idx in range(100):
     bucket.put_object(
