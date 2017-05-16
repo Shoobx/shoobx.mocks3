3b9dd6c9172325a839bfdede999856082ebd109d
Stephan Richter <stephan.richter@gmail.com>
2017-05-15 21:14:36 -0400

    Get ready for release.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(fname):
 
 setup(
     name="shoobx.mocks3",
-    version='0.1.0.dev0',
+    version='1.0.0.dev0',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="Shoobx Mock S3 Implementation",
