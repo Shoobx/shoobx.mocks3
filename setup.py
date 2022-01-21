83b192d8087313c91ed143116403d24cb1834953
Stephan Richter <stephan.richter@gmail.com>
2022-01-21 08:32:18 -0500

    Back to development: 2.1.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(fname):
 
 setup(
     name="shoobx.mocks3",
-    version='2.1.0',
+    version='2.1.1.dev0',
     author="Shoobx, Inc.",
     author_email="dev@shoobx.com",
     description="Shoobx Mock S3 Implementation",
